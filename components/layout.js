4f3ad938857a8d603337cd7e5a4da5777561b439
didierganthier <didierganthierperan@gmail.com>
2021-12-26 07:34:29 -0500

    latest setup for blog

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
 const name = 'Didier Peran Ganthier'
-export const siteTitle = 'Next.js Sample Website'
+export const siteTitle = 'Didier Peran Ganthier'
 
 export default function Layout({ children, home }) {
   return (
